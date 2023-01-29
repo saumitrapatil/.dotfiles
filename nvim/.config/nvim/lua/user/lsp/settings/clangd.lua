33902703ab8477fcb52e04874116d28db563a7d1
SaumitraPatil <saumitrapatil2703@gmail.com>
2023-01-29 13:22:28 +0530

    .

--- nvim/.config/nvim/lua/user/lsp/settings/clangd.lua
+++
@@ -1,5 +0,0 @@
-local capabilities = vim.lsp.protocol.make_client_capabilities()
-capabilities.offsetEncoding = { "utf-8" }
-return {
-	filetypes = { "c", "cpp", "objc", "objcpp", "cuda", "proto" },
-}
